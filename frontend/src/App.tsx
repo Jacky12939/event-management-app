07e63797b6424f1bdcdb25d4c787709f744994ca
Jacky12939 <jacquelinetagne6@gmail.com>
2026-05-30 11:28:48 +0100

    fix: resolution des conflits

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,21 @@
-// =============================================================
 // src/App.tsx
-// =============================================================
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-<<<<<<< HEAD
 import Events from "./pages/Events";
 import EventsPage from "./pages/participant/EventsPage";
-
-
-
+import CheckerPage from "./pages/checkerPage";
+import MyTicket from "./pages/MyTicket";
+import OrganizerPage from "./pages/OrganizerPage";
 
 
 export default function App(){
-
-  return(
-
-=======
-import CheckerPage from "./pages/checkerPage"; // ← nouveau
-import MyTicket from "./pages/MyTicket";
-import OrganizerPage from "./pages/OrganizerPage";
-export default function App() {
   return (
->>>>>>> 739e56dbd0161c8570bab61bf1516e4c899b1f03
     <BrowserRouter>
       <Routes>
-<<<<<<< HEAD
 
         <Route
           path="/"
@@ -60,15 +47,9 @@ export default function App() {
           element={<EventsPage/>}
         />
 
-=======
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/checker" element={<CheckerPage />} />
         <Route path="/my-tickets" element={<MyTicket />} />
         <Route path="/organisateur" element={<OrganizerPage />} />
->>>>>>> 739e56dbd0161c8570bab61bf1516e4c899b1f03
       </Routes>
     </BrowserRouter>
   );
